b9cfa171fe5d7952ef1a7fbe369c32ff4c840c9d
lujiajie <3454181311@qq.com>
2023-06-24 20:20:55 +0800

    home

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,5 +1,4 @@
-// import a from './a';
-// import './home.less';
+import './home.css';
 // import axios from 'axios';
 // axios
 //   .get('/bff/app-provision-api/default/banner', {
@@ -24,4 +23,16 @@ right.addEventListener('click', function () {
   if (parseInt(uls.style.left) > -225) {
     uls.style.left = parseInt(uls.style.left) - 75 + 'vw';
   }
+});
+
+var swiper = new Swiper('.mySwiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // 设置自动轮播
+  autoplay: {
+    delay: 2000, // 轮播间隔时间，单位为毫秒
+  },
 });
