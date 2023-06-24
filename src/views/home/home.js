e0332511bb9568d477caa7f57c887849a832c739
lujiajie <3454181311@qq.com>
2023-06-24 21:58:45 +0800

    home

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,13 +1,4 @@
-// import a from './a';
-// import './home.less';
-// import axios from 'axios';
-// axios
-//   .get('/bff/app-provision-api/default/banner', {
-//     params: {
-//       bannerType: 'tmall-banner',
-//     },
-//   })
-//   .then((res) => console.log(res));
+import './home.less';
 
 var imgsx = document.querySelectorAll('li>img');
 var uls = document.querySelector('ul');
@@ -24,4 +15,16 @@ right.addEventListener('click', function () {
   if (parseInt(uls.style.left) > -225) {
     uls.style.left = parseInt(uls.style.left) - 75 + 'vw';
   }
+});
+
+var swiper = new Swiper('.mySwiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // 设置自动轮播
+  autoplay: {
+    delay: 2000, // 轮播间隔时间，单位为毫秒
+  },
 });
